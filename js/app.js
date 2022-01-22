fea91e7425ea1d86726f0c978ce0596ad655ab1a
Ozod Abdusamatov <oabdusamatov@gmail.com>
2022-01-22 21:10:51 +0500

    Finished Slider and Join section

--- js/app.js
+++ js/app.js
@@ -17,6 +17,7 @@ window.onclick = function(event) {
   }
 }
 
+/*--------------------------------------------------------------------------------------*/
 
 //Active
 
@@ -25,6 +26,24 @@ let items = itemContainer.getElementsByClassName('grid-box__item');
 // Loop through the buttons and add the active class to the current/clicked button
 for (var i = 0; i < items.length; i++) {
   items[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    // If there's no active class
+    if (current.length > 0) {
+      current[0].className = current[0].className.replace(" active", "");
+    }
+
+    // Add the active class to the current/clicked button
+    this.className += " active";
+  });
+}
+
+
+//Newcomer - section
+let elemContainer = document.getElementById('newcomer__grid-box');
+let elements = elemContainer.getElementsByClassName('newcomer__item');
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < elements.length; i++) {
+  elements[i].addEventListener("click", function() {
     var current = document.getElementsByClassName("active");
 
     // If there's no active class
@@ -36,5 +55,41 @@ for (var i = 0; i < items.length; i++) {
     this.className += " active";
   });
 }
+/*--------------------------------------------------------------------------------------*/
 
 //Heart
+
+/*--------------------------------------------------------------------------------------*/
+
+//Slider
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("blogSlides");
+  let dots = document.getElementsByClassName("blogSlider__dots--dot");
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" current", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " current";
+}
